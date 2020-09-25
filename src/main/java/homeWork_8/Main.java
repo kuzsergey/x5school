5c1532bb9fa87314fc6ecece6c19924409bc8048
kuzsergey <kuzsergey2020@mail.ru>
2020-09-25 10:55:42 +0300

    Commit

--- src/main/java/homeWork_8/Main.java
+++ src/main/java/homeWork_8/Main.java
@@ -1,16 +1,23 @@
 package homeWork_8;
 
-
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws homeWork_8.UnknownAccountException {
+    public static void main(String[] args) {
         try {
             Class.forName("org.h2.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
+        }
+        File file = new File("./schema.sql");
+        if (!file.exists()) {
+            createFile(file);
         }
         String url = "jdbc:h2:mem:test;INIT=RUNSCRIPT FROM './schema.sql'";
 
@@ -21,6 +28,21 @@ public class Main {
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void createFile(File file) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(file, false))) {
+            file.createNewFile();
+            String string = "DROP TABLE IF EXISTS ACCOUNTS;\n" +
+                    "CREATE TABLE IF NOT EXISTS ACCOUNTS (\n" +
+                    "id VARCHAR(50) NOT NULL,\n" +
+                    "holder VARCHAR(50) NOT NULL,\n" +
+                    "amount INT\n" +
+                    ");\n";
+            writer.println(string);
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
